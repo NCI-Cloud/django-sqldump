5538a8dd13f6112d3df98f0f532cec3f4a95eca4
Malcolm Gill <malcolm.gill@gmail.com>
2015-07-13 07:16:14 +0000

    Output hosts table as xml or json.
    Originally tried to do this with django's built-in serialization, but
    found it a bit too limited. Our tables have multi-column primary keys,
    which django doesn't know how to handle, and which confused the
    serializers. Working around this limitation for the json serializer was
    doable (by subclassing it and overriding get_dump_object to to force the
    natural_key to be used) but the xml serializer was more complicated and
    it seemed easier just to write a few lines of django template to do it.

--- views.py
+++ views.py
@@ -1,4 +1,19 @@
+from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
+import json
 
 def index(request):
-    return HttpResponse('hallo')
+    return HttpResponse('have some options')
+
+def hosts(request):
+    data = Hosts.objects.all()[:10] # TODO get rid of this eventually...
+
+    t = request.GET.get('type') # TODO maybe should come from http header
+    if t == 'json':
+        out = json.dumps([{field.attname : getattr(h, field.attname) for field in h._meta.get_fields()} for h in data], indent=None)
+        return HttpResponse(out, content_type='application/json')
+
+    # return xml by default
+    context = {'hosts' : data}
+    return render(request, 'dump/hosts.xml', context, content_type='application/xml')
