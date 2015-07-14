d48b27ef56fe91d3206d777334404d0733d0de87
Malcolm Gill <malcolm.gill@gmail.com>
2015-07-14 14:46:36 +1000

    Include dtd in xml output.

--- views.py
+++ views.py
@@ -4,6 +4,7 @@ from django.db import connections
 from .models import *
 import json
 from lxml import etree
+from django.conf import settings
 
 class Query(object):
     def __init__(self, sql, root='list', row='object'):
@@ -18,8 +19,8 @@ class Query(object):
         """Return [{col1:val1, col2:val2, ...} ...]."""
         cursor = connections['stats'].cursor() # TODO fix this hard-coded string everywhere
         cursor.execute(self.sql)
-        cols = [col[0] for col in cursor.description]
-        return [{col:val for (col, val) in zip(cols, row)} for row in cursor.fetchall()]
+        self.cols = [col[0] for col in cursor.description]
+        return [{col:val for (col, val) in zip(self.cols, row)} for row in cursor.fetchall()]
 
     def get_as(self, f):
         """Return results as string, formatted as f. Raise ValueError for unknown f."""
@@ -27,16 +28,45 @@ class Query(object):
         if f == 'xml':
             root = etree.Element(self.root)
             for row in rows:
-                print(row)
                 root.append(etree.Element(self.row, **{k:str(row[k]) for k in row}))
-            return etree.tostring(root, pretty_print=True)
+
+            #root.append(etree.Element('rabs', **{'español':'hi'}))
+            out = etree.tostring(root, pretty_print=True, encoding='utf-8')
+            p = etree.XMLParser(dtd_validation=False)
+            tst = etree.fromstring(out, p)
+
+            # can get actual field types by inspecting cursor.description,
+            # but these (I think) depend on the database back-end
+            # so I decided it was too hard to be smart here,
+            # and instead just say that everything is "CDATA #REQURED"...
+            attlist = '\n'.join('  {col} CDATA #REQUIRED'.format(col=col) for col in self.cols)
+
+            # generating dtd means we can automatically validate output, for testing
+            dtd = '<!DOCTYPE {root} [\n' \
+                  ' <!ELEMENT {root} ({row})*>\n' \
+                  ' <!ELEMENT {row} EMPTY>\n' \
+                  ' <!ATTLIST {row}\n{attlist}\n' \
+                  ' >\n' \
+                  ']>'.format(root=self.root, row=self.row, attlist=attlist)
+
+            # DEFAULT_CHARSET is used by HttpResponse, so make etree use it too.
+            # Output may become invalid if DEFAULT_CHARSET cannot be used to encode field names!
+            # e.g. MySQL columns may include characters from Unicode Basic Multiingual Plane,
+            # which could be inexpressible if DEFAULT_CHARSET were ascii, giving invalid xml.
+            return etree.tostring(
+                root,
+                pretty_print    = False,
+                encoding        = settings.DEFAULT_CHARSET,
+                xml_declaration = True,
+                doctype         = dtd,
+            )
         elif f == 'json':
             return json.dumps(rows, indent=2)
         else:
             raise ValueError('unknown format "{0}"'.format(f))
 
 # TODO this should be in a config file somewhere
-# keys must match \w+
+# keys must match \w+ (because of specification in urls.py)
 QUERIES = {
     'hallo' : Query(
         sql  = "SELECT * FROM hosts LIMIT 10",
