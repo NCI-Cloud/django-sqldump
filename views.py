ec46f5609936f89721094670b32f440496329cb6
Malcolm Gill <malcolm.gill@gmail.com>
2015-07-14 14:57:00 +1000

    Remove models for hosts and instances.
    The dump app will only work directly with sql, not with any higher-level
    abstractions.

--- views.py
+++ views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.db import connections
-from .models import *
 import json
 from lxml import etree
 from django.conf import settings
@@ -86,15 +85,3 @@ def query(request, query_key):
 
 def index(request):
     return HttpResponse('have some options') # TODO implement
-
-def hosts(request):
-    data = Hosts.objects.all()[:10] # TODO get rid of this eventually...
-
-    t = request.GET.get('type') # TODO maybe should come from http header
-    if t == 'json':
-        out = json.dumps([{field.attname : getattr(h, field.attname) for field in h._meta.get_fields()} for h in data], indent=None)
-        return HttpResponse(out, content_type='application/json')
-
-    # return xml by default
-    context = {'hosts' : data}
-    return render(request, 'dump/hosts.xml', context, content_type='application/xml')
