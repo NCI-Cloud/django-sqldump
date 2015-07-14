ea9b02da568732569e16404cb0c79b3bc72613fe
Malcolm Gill <malcolm.gill@gmail.com>
2015-07-14 10:42:03 +1000

    Implement general query output.
    This is messy and in some places hacky, but at least it's working...

--- urls.py
+++ urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^hosts/', views.hosts),
+    url(r'^q/(?P<query_key>\w+)/', views.query),
 ]
--- views.py
+++ views.py
@@ -1,10 +1,61 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.db import connections
 from .models import *
 import json
+from lxml import etree
+
+class Query(object):
+    def __init__(self, sql, root='list', row='object'):
+        """
+            sql    - query to be executed
+            root   - name of root element, for xml output
+            row    - name of each record, for xml output
+        """
+        self.sql, self.root, self.row = sql, root, row
+
+    def get(self):
+        """Return [{col1:val1, col2:val2, ...} ...]."""
+        cursor = connections['stats'].cursor() # TODO fix this hard-coded string everywhere
+        cursor.execute(self.sql)
+        cols = [col[0] for col in cursor.description]
+        return [{col:val for (col, val) in zip(cols, row)} for row in cursor.fetchall()]
+
+    def get_as(self, f):
+        """Return results as string, formatted as f. Raise ValueError for unknown f."""
+        rows = self.get()
+        if f == 'xml':
+            root = etree.Element(self.root)
+            for row in rows:
+                print(row)
+                root.append(etree.Element(self.row, **{k:str(row[k]) for k in row}))
+            return etree.tostring(root, pretty_print=True)
+        elif f == 'json':
+            return json.dumps(rows, indent=2)
+        else:
+            raise ValueError('unknown format "{0}"'.format(f))
+
+# TODO this should be in a config file somewhere
+# keys must match \w+
+QUERIES = {
+    'hallo' : Query(
+        sql  = "SELECT * FROM hosts LIMIT 10",
+        root = 'hosts',
+        row  = 'host',
+    ),
+}
+
+def query(request, query_key):
+    if query_key not in QUERIES:
+        raise Http404('unknown query key')
+
+    f = request.GET.get('format', default='xml') # TODO maybe should come from http header
+
+    ct = {'json' : 'application/json', 'xml' : 'application/xml'} # TODO clean up
+    return HttpResponse(QUERIES[query_key].get_as(f), content_type=ct[f])
 
 def index(request):
-    return HttpResponse('have some options')
+    return HttpResponse('have some options') # TODO implement
 
 def hosts(request):
     data = Hosts.objects.all()[:10] # TODO get rid of this eventually...
