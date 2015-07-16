204b6e2844221bfada28fa2c442678124b25d290
Malcolm Gill <malcolm.gill@gmail.com>
2015-07-16 16:46:05 +1000

    Test handling of http accept header.

--- tests.py
+++ tests.py
@@ -1,11 +1,47 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
 from django.db import connections
 from django.db import models
 from dump.models import Query
 from .settings import TEST_DB_CON
-from .views import get_xml, get_json
+from .views import get_query_xml, get_query_json
+from .models import Query
 from lxml import etree
 import json
+
+class ViewsTests(TestCase):
+    def setUp(self):
+        self.qkey = 'reflect'
+        Query(
+            key=self.qkey,
+            sql='SELECT * FROM {0}'.format(Query._meta.db_table),
+            root='queries',
+            row='query',
+        ).save()
+
+    def test_404(self):
+        response = Client().get(reverse('query', args=(None,)))
+        self.assertEqual(response.status_code, 404, 'should hit 404 for invalid query key')
+
+    def test_accepts(self):
+        ct = 'application/xml'
+        cu = 'application/json'
+
+        # test sending single content type in http header
+        c = Client(HTTP_ACCEPT='{content_type}'.format(content_type=ct))
+        response = c.get(reverse('query', args=(self.qkey,)))
+        self.assertTrue(response.has_header('content-type'), msg='http response needs content-type')
+        self.assertEqual(response['content-type'], ct, msg='should receive content-type "{0}" when requesting it'.format(ct))
+
+        # test sending multiple content types
+        c = Client(HTTP_ACCEPT='{other_type}; q=0.2, {content_type}'.format(content_type=ct, other_type=cu))
+        response = c.get(reverse('query', args=(self.qkey,)))
+        self.assertEqual(response['content-type'], ct, msg='should receive content-type "{0}" when requesting it with higher precedence'.format(ct))
+
+        # test overriding with query parameter
+        c = Client(HTTP_ACCEPT=cu)
+        response = c.get(reverse('query', args=(self.qkey,)), {'accept':ct})
+        self.assertEqual(response['content-type'], ct, msg='should receive content-type "{0}" when passing in url'.format(ct))
 
 class ValidationTests(TestCase):
     """
@@ -101,11 +137,11 @@ class ValidationTests(TestCase):
         self.Citizen.objects.all().delete()
 
     def test_json_empty(self):
-        citizens = json.loads(get_json(self.citizen_query))
+        citizens = json.loads(get_query_json(self.citizen_query))
         self.assertEqual(citizens, [], msg='got nonempty json result from empty table')
 
     def test_xml_empty(self):
-        root = etree.fromstring(get_xml(self.citizen_query))
+        root = etree.fromstring(get_query_xml(self.citizen_query))
         self.assertEqual(len(root), 0, msg='got nonempty xml result from empty table')
 
     def test_json_nonempty(self):
@@ -113,7 +149,7 @@ class ValidationTests(TestCase):
         original_citizens = [self.get_citizen(), self.get_bobby()]
         for c in original_citizens:
             self.insert_citizen(c)
-        rows = json.loads(get_json(self.citizen_query))
+        rows = json.loads(get_query_json(self.citizen_query))
         retrieved_citizens = [self.Citizen(**att) for att in rows]
 
         # N.B. this relies on the rows being retrieved in the order they were created
@@ -125,14 +161,14 @@ class ValidationTests(TestCase):
         original_citizens = [self.get_citizen(), self.get_bobby()]
         for c in original_citizens:
             self.insert_citizen(c)
-        root = etree.fromstring(get_xml(self.citizen_query))
+        root = etree.fromstring(get_query_xml(self.citizen_query))
         retrieved_citizens = [self.Citizen(**row.attrib) for row in root]
         for (retrieved, original) in zip(retrieved_citizens, original_citizens):
             self.compare_citizens(retrieved, original)
 
     def test_xml_rootrow(self):
         self.insert_citizen()
-        root = etree.fromstring(get_xml(self.citizen_query))
+        root = etree.fromstring(get_query_xml(self.citizen_query))
         self.assertEqual(root.tag, self.citizen_query.root, msg='got different root tag name')
         self.assertEqual(root[0].tag, self.citizen_query.row, msg='got different row tag name')
 
@@ -142,4 +178,4 @@ class ValidationTests(TestCase):
         self.insert_citizen(self.get_bobby())
 
         parser = etree.XMLParser(dtd_validation=True)
-        root = etree.fromstring(get_xml(self.citizen_query), parser) # raises exception on failure
+        root = etree.fromstring(get_query_xml(self.citizen_query), parser) # raises exception on failure
